b76c90791f2053cca5573ae95a476de9cb308f04
Wassim <Wassim.lahbibi@gmail.com>
2018-12-08 14:30:10 +0100

    reconfig

---
+++ hackathon/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Home from './compenents/Home';
+
+const App = () => (
+  <div className="App">
+    <Home />
+  </div>
+);
+
+export default App;
