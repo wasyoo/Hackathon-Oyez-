d2b11b1524e28fe2eb6ba4ef31b75adbe7fa4a72
Rammoun <44053646+Rammoun@users.noreply.github.com>
2018-12-09 08:41:02 +0100

    add apollo client

--- hackathon/src/index.jsx
+++ hackathon/src/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from 'react-apollo';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const client = new ApolloClient({ uri: "localhost:4000"})
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
